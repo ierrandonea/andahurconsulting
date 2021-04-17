903eaab37c5015ff03810d4b3335934545aaece7
ierrandonea <ierrandoneag@gmail.com>
2021-04-16 20:44:19 -0400

    final review && favicon add

--- src/Sections/subsections/Services.js
+++ src/Sections/subsections/Services.js
@@ -19,24 +19,25 @@ const Services = () => {
                     <div className="col-12 mb-3">
                         <h4>Consultoría TI</h4>
                         <p>
-                            <strong className="text-emphasis fw-500">Asesoro</strong> en múltiples áreas relacionadas a la gestión de proyectos y sistemas, identificando y proponiendo estrategias efectivas que impacten en el negocio y en la consecucion de los objetivos esperados por cada organización, gestionando tanto factores de valor como disminuyendo la brecha en factores higiénicos. Mi orientación en este ámbito está en identificar nuevas oportunidades, que no necesariamente son parte del “core” del negocio, pero tienen el potencial de mejorar sustancialmente el funcionamiento del mismo.
-                            </p>
+                            <strong className="text-emphasis fw-500">Asesoro</strong> en múltiples áreas relacionadas a la gestión de proyectos y sistemas, identificando y proponiendo estrategias efectivas que impacten en el negocio y en la consecución de los objetivos esperados por cada organización, gestionando tanto factores de valor como disminuyendo la brecha en factores higiénicos. Mi orientación en este ámbito está en identificar nuevas oportunidades, que no necesariamente son parte del “core” del negocio, pero que tienen el potencial de mejorar sustancialmente el funcionamiento del mismo.
+                        </p>
                     </div>
                     <div className="col-12 mb-3">
                         <h4>Implementación de Proyectos</h4>
                         <p>
-                            <strong className="text-emphasis fw-500">Gestiono</strong> equipos de trabajo enfocándome en aprovechar el potencial individual de cada miembro del equipo, de cara a lograr los objetivos de cada proyecto de la mejor forma posible. Utilizo tanto estándares PMI como metodologías Agile, liderando equipos multidisciplinarios y facilitanto celulas de trabajo especializadas según los requerimientos de cada proyecto.
+                            <strong className="text-emphasis fw-500">Gestiono</strong> equipos de trabajo enfocándome en aprovechar el potencial individual de cada miembro del equipo, de cara a lograr los objetivos de cada proyecto de la mejor forma posible. Utilizo tanto estándares PMI como metodologías Agile, liderando equipos multidisciplinarios y facilitando células de trabajo especializadas, según los requerimientos de cada proyecto.
                         </p>
                     </div>
                     <div className="col-12 mb-3">
                         <h4>Sistemas de Gestión</h4>
                         <p>
-                            <strong className="text-emphasis fw-500">Audito</strong> internamente bajo normas ICE/ISO, apoyando al cliente en la mejora de sus procesos, aprovechando al máximo los recursos disponibles y potenciando las labores que se realizan. También gestiono la planificación y ejecución de proyectos para certificaciones, ofreciendo soluciones de diagnóstico y mejora continua.</p>
+                            <strong className="text-emphasis fw-500">Audito</strong> internamente bajo normas ICE/ISO, apoyando al cliente en la mejora de sus procesos, aprovechando al máximo los recursos disponibles y potenciando las labores que se realizan. También gestiono la planificación y ejecución de proyectos para certificaciones, ofreciendo soluciones de diagnóstico y mejora continua.
+                        </p>
                     </div>
                     <div className="col-12">
                         <h4>PMO</h4>
                         <p>
-                            <strong className="text-emphasis fw-500">Apoyo</strong> y complemento la gestión de los jefes de proyecto, agilizando los procesos de negociación y comunicación con stakeholders, ofreciendo una nueva perspectiva que ayude a comprender el proyecto y sus implicaciones para sus clientes tanto internos como externos. Desarrollo estrategias de mitigación y matrices de riesgo, facilitando actividades y velando por la actualización de documentos y lecciones aprendidas.
+                            <strong className="text-emphasis fw-500">Apoyo</strong> y complemento la gestión de los jefes de proyecto, agilizando los procesos de negociación y comunicación con stakeholders, ofreciendo una nueva perspectiva que ayude a comprender el proyecto y sus implicaciones para sus clientes internos y externos. Desarrollo estrategias de mitigación y matrices de riesgo, facilitando actividades y velando por la actualización de documentos y lecciones aprendidas.
                         </p>
                     </div>
                 </div>
